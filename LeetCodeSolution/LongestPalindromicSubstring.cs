bf2c61b02cb9c9f7ad2d9302f867fc101c9a2049
Ren Zhiwei <renzhiwei@outlook.com>
2019-06-08 08:26:10 +0800

    initialize

---
+++ LeetCodeSolution/LongestPalindromicSubstring.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+
+namespace Bigman.LeetCodeSolution
+{
+    public class LongestPalindromicSubstring
+    {
+        public string RunOne(string source)
+        {
+            string longestPalindromicSubstring;
+            return source;
+        }
+
+        public bool IsPalindromic(string source, int startIndex, int endIndex)
+        {
+            if(endIndex>startIndex){
+                
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCodeSolutionTests/LongestPalindromicSubstringTest.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using Bigman.LeetCodeSolution;
+using Bigman.LeetCodeSolutionTests.Utilities;
+
+namespace Bigman.LeetCodeSolutionTests
+{
+    public class LongestPalindromicSubstringTest
+    {
+        [Theory]
+        [InlineData("cbbd","bb")]
+        [InlineData("babad","bab")]
+        public void Test_Algorithm_one(string source, string expect)
+        {
+            var algorithm = new LongestPalindromicSubstring();
+
+            var actual = algorithm.RunOne(source);
+
+            Assert.Equal(expect, actual);
+        }
+    }
+}
