709087480116dda123edd68de7c662dd9b2f6287
Ren Zhiwei <renzhiwei@outlook.com>
2019-06-09 12:35:32 +0800

    first implemention of the problem

--- LeetCodeSolution/LongestPalindromicSubstring.cs
+++ LeetCodeSolution/LongestPalindromicSubstring.cs
@@ -7,14 +7,28 @@ namespace Bigman.LeetCodeSolution
     {
         public string RunOne(string source)
         {
-            string longestPalindromicSubstring;
-            return source;
+            string longestPalindromicSubstring="";
+
+            for(int i=0; i< source.Length; i++)
+            {
+                for(int j=i+longestPalindromicSubstring.Length; j< source.Length; j++){
+                    if ( IsPalindromic(source,i,j) && j-i+1>longestPalindromicSubstring.Length){
+                        longestPalindromicSubstring = source.Substring(i,j-i+1);
+                    }
+                }
+            }
+
+            return longestPalindromicSubstring;
         }
 
         public bool IsPalindromic(string source, int startIndex, int endIndex)
         {
-            if(endIndex>startIndex){
-                
+            for (int i = 0; i < endIndex - startIndex; i++)
+            {
+                if (source[startIndex + i] != source[endIndex - i])
+                {
+                    return false;
+                }
             }
             return true;
         }
